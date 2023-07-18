3351290a51aefc0f0b9e74778208b73ed760aed8
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-07-18 10:00:39 +0100

    added colors to navbar and a more contained form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  
+    constructor(public router: Router) { }
+
+  
 
 }
